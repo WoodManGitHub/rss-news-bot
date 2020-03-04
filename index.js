0023787a1added3673b546a6300f7f6f8d4430a3
WoodManGitHub <woodmanpublic@gmail.com>
2020-03-04 22:10:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const RssFeedEmitter = require('rss-feed-emitter')
+const feeder = new RssFeedEmitter()
+const config = require('./config.json')
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(config.telegram.token, { polling: true })
+
+for (let list of config.webList) {
+    feeder.add({
+        url: list,
+        refresh: 2000
+    })
+}
+
+feeder.on('new-item', (item) => {
+    const text = `${item.meta.title}\n${item.title}\n LINK:\n ${item['rss:link']['#']}`
+    bot.sendMessage(config.telegram.chatID, text)
+})
