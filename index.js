12da8dc942ab8a46859f9a2fa60dd0bebf9a6466
WoodManGitHub <woodmanpublic@gmail.com>
2020-03-09 11:10:10 +0800

    Add message queue

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const RssFeedEmitter = require('rss-feed-emitter')
-const feeder = new RssFeedEmitter()
 const config = require('./config.json')
 const TelegramBot = require('node-telegram-bot-api')
+const Queue = require('promise-queue')
 
+const feeder = new RssFeedEmitter()
+const messageQueue = new Queue(1)
 const bot = new TelegramBot(config.telegram.token, { polling: true })
 
 for (let list of config.webList) {
@@ -13,6 +15,19 @@ for (let list of config.webList) {
 }
 
 feeder.on('new-item', (item) => {
-    const text = `${item.meta.title}\n${item.title}\nLINK:\n${item['rss:link']['#']}`
-    bot.sendMessage(config.telegram.chatID, text)
+    const text = `${item.meta.title}\n${item.title}\n${item['rss:link']['#']}`
+
+    queueSendMessage(config.telegram.chatID, text)
 })
+
+const queueSendMessage = (chatID, text) => {
+    return messageQueue.add(async () => {
+        const callback = bot.sendMessage(chatID, text)
+        await sleep(10000)
+        return callback
+    })
+}
+
+const sleep = (time) => {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
